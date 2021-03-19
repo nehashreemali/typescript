8495db1b21c1afcd22d81686c04917e0b4428957
neha <79583017+nehatrivedieducrack@users.noreply.github.com>
2021-03-19 10:52:23 +0530

    my project

---
+++ index.ts
@@ -0,0 +1,8 @@
+class Departmemnt {
+name:string;
+constructor(n:string){
+    this.name=n;
+}
+}
+const acnt=new Departmemnt('neha')
+console.log(acnt);
---
+++ src/index.js
@@ -0,0 +1,8 @@
+"use strict";
+class Departmemnt {
+    constructor(n) {
+        this.name = n;
+    }
+}
+const acnt = new Departmemnt('neha');
+console.log(acnt);
